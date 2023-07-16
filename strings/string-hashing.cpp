a0036468342a3ae57a3b5e928ebff44de7625798
Siddharth Ruria <siddharthruria10@gmail.com>
2023-07-16 15:48:04 +0530

    edit string-hashing.cpp

--- strings/string-hashing.cpp
+++ strings/string-hashing.cpp
@@ -1,26 +1,6 @@
 // Siddharth Ruria
 // Linkedin: https://www.linkedin.com/in/ruria-siddharth/
 
-/* helps calculate hash of a substring in constant time. */
-// working explained: 
-/* - add(), multiply(), and power() are just helper functions which are modular operations to help keep the operations under modulo (1e9+7).
-   - the pre_computation() is only called once throughout the entire lifetime of the code.
-   - build() function is called once for one string. it helps create the array hashes[] of a string, where hashes[i] indicates the hash of the substring from 0 -> i  S[0, i].
-     if you want to create hashes[] array for a different string, you need to call build() for that as well.
-   - get_hash() is the main function which is called again and again, how many times you need to retrieve the hash of whatever part of the string. it works in O(1) time.
-   - the pre_computation() and build() are only for some before hand calculations which helps us make the get_hash() function work in constant time.
-   - the working of the code is based on some mathematical calculations I have done which also involves the Fermat's Little Theorem. 
-     the proof and working of the code is 100% accurate and correct.
-   */
-
-/* extra notes:
-    1. 'base' should be prime and greater than the number of characters in the input string
-    2. 'mod' should also be prime.*/
-
-/* updates required:
-   - need to randomise the values of 'mod' and 'base' so as to avoid hacking in a live environment (CF contests).
-   - could add another modulo M2 and perform double hashing. */
-
 #include <iostream>
 #include <bits/stdc++.h>
 
@@ -82,8 +62,11 @@ void pre_computation() {
 // creates hashes of the string from i= 0 -> (N-1) [only runs once for a string S]
 void build(string s) {
 	int n= s.length();
-	for(int i= 1; i< n; ++i) {
-		hashes[i]= add((i== 0) ? 0 : hashes[i-1], multiply(s[i]-'a'+1, pw[i], mod), mod);
+	for(int i= 0; i< n; ++i) {
+		int char_val= s[i]-'a'+1;
+		int mult= multiply(char_val, pw[i], mod);
+		if(i== 0) hashes[i]= add(0, mult, mod);
+		else hashes[i]= add(hashes[i-1], mult, mod);
 	}
 }
 
