b930cbbe1a8acfbd466b721e9c45a87f05d9b9cd
Siddharth Ruria <siddharthruria10@gmail.com>
2023-07-16 11:08:18 +0530

    add string-hashing.cpp
    template for hashing a string.

---
+++ strings/string-hashing.cpp
@@ -0,0 +1,80 @@
+// Siddharth Ruria
+// Linkedin: https://www.linkedin.com/in/ruria-siddharth/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// constants
+const int N = 1e6+6;
+const int mod= 1e9+7;
+const int base= 31;
+
+// modular operations
+int add(int a, int b, int mod) {
+	int res= (a+b)%mod;
+	if(res< 0) {
+		res+= mod;
+	}
+	return res;
+}
+
+int multiply(int a, int b, int mod) {
+	int res= (a*1LL*b)%mod;
+	if(res< 0) {
+		res+= mod;
+	}
+	return res;
+}
+
+int power(int a, int b, int mod) {
+	int res= 1;
+	while(b) {
+		if((b%2)== 1) 
+			res= multiply(res, a, mod);
+		a= multiply(a, a, mod);
+		b>>= 1;
+	}
+	return res;
+}
+
+// storage arrays
+int pw[N];
+int inv[N];
+int hashes[N];
+
+// runs only once throughout the lifetime of the code
+void pre_computation() {
+	// calculates powers of base upto N
+	pw[0]= 1;
+	for(int i= 1; i< N; ++i) 
+		pw[i]= multiply(pw[i-1], base, mod);
+
+	// calculates inverse powers of base upto N
+	int base_inverse= power(base, mod-2, mod);
+	inv[0]= 1;
+	for(int i= 1; i< N; ++i) {
+		inv[i]= multiply(inv[i-1], base_inverse, mod);
+	}
+}
+
+// creates hashes of the string from i= 0 -> (N-1) [only runs once for a string S]
+void build(string s) {
+	int n= s.length();
+	for(int i= 1; i< n; ++i) {
+		hashes[i]= add((i== 0) ? 0 : hashes[i-1], multiply(s[i]-'a'+1, pw[i], mod), mod);
+	}
+}
+
+// returns the hash of the substring S[x, y] => H(S[x....y])
+int get_hash(int x, int y) {
+	int res= add(hashes[y], (x== 0)? 0: -hashes[x-1], mod);
+	res= multiply(res, inv[x], mod);
+	return res;
+}
+
+// main function
+int main() {
+	pre_computation();
+}
