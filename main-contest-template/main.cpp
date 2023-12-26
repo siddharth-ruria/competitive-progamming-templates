b9206f38c73111a6821e576ccf0cd61803d30f05
Siddharth Ruria <siddharthruria10@gmail.com>
2023-12-26 16:24:21 +0530

    Update main.cpp

--- main-contest-template/main.cpp
+++ main-contest-template/main.cpp
@@ -56,12 +56,12 @@ ostream& operator <<(ostream &ostream, const vector<T> &c) { for(auto &it: c) co
 
 // mathematical functions
 ll moduloMultiplication(ll a,ll b,ll mod){ ll res= 0; a%= mod; while(b) { if(b&1) res= (res+a)%mod; b>>= 1; } return res; }
-ll moduloPower(ll x, ll y, ll p){ ll res= 1; x= x%p; if (x== 0) return 0; while (y> 0){ if(y&1) res= (res*x)%p; y = y>> 1; x= (x*x)%p; } return res; }
+ll moduloPower(ll x, ll y, ll p){ ll res= 1; x= x%p; if (x== 0) return 0; while (y> 0){ if(y&1) res= (res*x)%p; y= y>> 1; x= (x*x)%p; } return res; }
 
 // checks
 bool isPrime(ll n){ if(n<= 1) return false; if(n<= 3) return true; if(n%2== 0 || n%3== 0) return false; for(int i= 5; i*i<= n; i= i+6) if(n%i ==0 || n%(i+2)== 0) return false; return true; }
 bool isPowerOfTwo(ll n){ if(n== 0) return false; return (ceil(log2(n))== floor(log2(n))); }
-bool isPerfectSquare(ll x){ if(x>= 0) {ll sr= sqrt(x); return (sr*sr== x); } return false; }
+bool isPerfectSquare(ll x){ if(x>= 0) { ll sr= sqrt(x); return (sr*sr== x); } return false; }
 
 #define siddharth 1
 
@@ -70,7 +70,7 @@ bool isPerfectSquare(ll x){ if(x>= 0) {ll sr= sqrt(x); return (sr*sr== x); } ret
 #define debug(x) cerr << #x << " " << x << " " << nline;
 #else
 #define debug(x);
-#endif
+#endif	
 
 // code
 void solve() {
@@ -86,7 +86,7 @@ int main() {
 	auto start= high_resolution_clock::now();
 
 	int tt= 1;
-	// cin >> tt;
+	cin >> tt;
 	while(tt--) {
 		solve();
 	}
